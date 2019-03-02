5161a89ad7c804983c8f0eac083d4eda37d79ab8
yuanwl <weigesama@qq.com>
2019-03-02 10:37:06 +0800

    10-4

--- hystrix/readme.md
+++ hystrix/readme.md
@@ -6,3 +6,40 @@
 
 ![10-1-hystrix简介2](attachments/10-1-hystrix简介2.png)
 
+## 降级
+
+可以理解为: 调用某服务没有响应时, 转而调用一个设置好的降级方法, 给用户返回提示信息, 而不是一直等待响应--这个过程就叫降级.
+
+可使用 @HystrixCommand,@DefaultProperties 和降级方法实现降级. 触发降级的条件一般有:
+
+- 调用的服务不可用;
+- 调用的服务超时没有响应. 默认只等待1秒, 可通过在 @HystrixCommand 内的 @HystrixProperty 指定等待响应时间;
+- 主调用方法内部抛异常;
+
+代码请看: [HystrixController1.java](src/main/java/xyz/yuanwl/demo/spring/cloud/hystrix/controller/HystrixController1.java)
+
+## 熔断
+
+### 依赖隔离
+
+类似于 docker 的舱壁模式把进程隔离开来, hystrix 则把线程池隔离: 某个线程池失去响应, 不会拖慢其他线程池. hystrix 自动为加了 @HystrixCommand 的方法创建这样的线程池.
+
+### 容错
+
+为提高微服务的高可用, 必须容错. 一般有两种方式:
+
+1. 重试: 适用于预期只是短暂的故障, 重试几次就可能成功;
+2. 熔断: 如果故障是无法评估的, 可能是长时间, 就应该用这种模式(又叫断路器模式), 把调用服务过程封装在受监控的断路器对象里.
+
+### 熔断(断路器模式)
+
+首先要了解断路器状态机:
+
+![10-4-断路器模式状态机_1](attachments/10-4-断路器模式状态机_1.png)
+
+1. 断路器默认关闭;
+2. 监控: 每 n 次调用计算一次调用失败的比率, 如果在设置的百分比内, 依然关闭断路器, 否则打开断路器;
+3. 断路器打开后, 进入休眠时间窗口, 在这时间内, 所有调用都返回降级方法的结果, 即降级方法临时称为主调逻辑;
+4. 休眠时间结束后, 断路器进入半开状态, 如果有请求过来, 断路器就让请求到达主调用逻辑, 如果能调用, 就关闭断路器, 进入新一轮的监控计数; 否则, 断路器继续打开, 重新进入休眠时间窗口, 重新计时;
+
+代码请看: [HystrixController2.java](src/main/java/xyz/yuanwl/demo/spring/cloud/hystrix/controller/HystrixController2.java)
--- hystrix/src/main/java/xyz/yuanwl/demo/spring/cloud/hystrix/controller/HystrixController2.java
+++ hystrix/src/main/java/xyz/yuanwl/demo/spring/cloud/hystrix/controller/HystrixController2.java
@@ -4,53 +4,51 @@ import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
+ * 熔断演示
+ *
  * @author yuanwl
  * @date 2019-03-01 20:25
  */
 @RestController
-//加了下面这个注解, @HystrixCommand 不指定降级方法, 就调用这个默认降级方法
 @DefaultProperties(defaultFallback = "defaultFallback")
-public class HystrixController {
+public class HystrixController2 {
 
-//	@HystrixCommand//(fallbackMethod = "fallback") //调用指定降级方法
-	@HystrixCommand(commandProperties =
-		@HystrixProperty( //调用目标服务在3秒内有响应就不会触发降级
-				name = "execution.isolation.thread.timeoutInMilliseconds",
-				value = "3000")
-	)
-	@GetMapping("product/list")
-	public String productList() throws Exception {
+	@HystrixCommand(commandProperties = {
+			@HystrixProperty(name = "circuitBreaker.enabled", 
+					value = "true"), //启用熔断机制
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", 
+					value = "10"), //请求数达到后才计算是否应该熔断
+			@HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",
+					value = "60"), //错误率超过就打开断路器
+			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",
+					value = "5000"), //打开断路器后进入休眠时间窗, 结束后半开断路器
+	})
+	@GetMapping("product/list/2")
+	public String productList(@RequestParam("num") Integer num) throws Exception {
+		if (num % 2 == 0) {
+			return "success";
+		}
+		
 		RestTemplate restTemplate = new RestTemplate();
 
-		//目标服务不可用或者超时, 或本方法内部产生异常, 都会调用降级方法返回提示信息
 		return restTemplate.getForObject("http://localhost:8100/list",
 				String.class);
-//		throw new Exception();
-	}
-
-	/**
-	 * 降级方法
-	 * @return java.lang.String
-	 * @author Yuanwl
-	 * @date 2019-03-01 20:59:24
-	 * @version v1.0.0
-	 */
-	public String fallback(){
-		return "太拥挤了, 请稍后再试...";
 	}
 
 	/**
 	 * 默认降级方法
+	 *
 	 * @return java.lang.String
 	 * @author Yuanwl
 	 * @date 2019-03-01 20:59:44
 	 * @version v1.0.0
 	 */
-	public String defaultFallback(){
+	public String defaultFallback() {
 		return "默认: 太拥挤了, 请稍后再试...";
 	}
 }
